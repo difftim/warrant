50b356806188149a1ab17efdace3d1ccfe228490
lea <877180272@qq.com>
2025-07-02 22:08:42 +0800

    feat：fix sql语法问题

--- pkg/object/postgres.go
+++ pkg/object/postgres.go
@@ -206,7 +206,7 @@ func (repo PostgresRepository) List(ctx context.Context, filterOptions *FilterOp
 		WHERE
 			deleted_at IS NULL
 	`
-	query = fmt.Sprintf("%s AND org_id = %s", query, orgId)
+	query = fmt.Sprintf("%s AND org_id = '%s'", query, orgId)
 
 	replacements := []interface{}{}
 	primaryKeyColumn := sortRegexp.ReplaceAllString(PrimarySortKey, `_$1`)
