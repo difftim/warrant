45ffd7b174b600ea7a088e8bdecc38e47d7b6b43
lea <877180272@qq.com>
2025-07-04 14:43:10 +0800

    fix:sql参数格式问题

--- pkg/object/postgres.go
+++ pkg/object/postgres.go
@@ -625,7 +625,7 @@ func (repo PostgresRepository) selectPolicyGroupWarrantAppCount(ctx context.Cont
 		ctx,
 		&warrantAppCounts,
 		query,
-		strings.Join(objectIds, ","),
+		strings.Join(objectIds, "','"),
 	)
 	if err != nil {
 		return nil, errors.Wrap(err, "error selecting policy group warrant app count")
@@ -656,7 +656,7 @@ func (repo PostgresRepository) selectPolicyGroupWarrantUserCount(ctx context.Con
 		ctx,
 		&warrantUserCounts,
 		query,
-		strings.Join(objectIds, ","),
+		strings.Join(objectIds, "','"),
 	)
 	if err != nil {
 		return nil, errors.Wrapf(err, "error getting policy group warrant user counts for objects %v", objectIds)
